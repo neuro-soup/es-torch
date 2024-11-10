fb5461dc016f10db893205dde241e05a82666f08
Lukas Leeb <git@lukasl.dev>
2024-11-11 00:02:36 +0100

    fix import manually

--- es_torch/es/es_pb2_grpc.py
+++ es_torch/es/es_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-from es import es_pb2 as es_dot_es__pb2
+from es_torch.es import es_pb2 as es_dot_es__pb2
 
 GRPC_GENERATED_VERSION = '1.67.1'
 GRPC_VERSION = grpc.__version__
--- examples/grpc.py
+++ examples/grpc.py
@@ -6,6 +6,5 @@ import es_torch.es.es_pb2_grpc as es_pb2_grpc
 channel = grpc.insecure_channel('localhost:8080')
 stub = es_pb2_grpc.ESServiceStub(channel)
 
-hello_request = es_pb2.HelloRequest(num_cpus=4)
-hello_response = stub.Hello(hello_request)
-print("Hello Response:", hello_response.id, hello_response.state)
+resp: es_pb2.HelloResponse = stub.Hello(es_pb2.HelloRequest(num_cpus=4))
+print(resp)
