1db7346d0f6fb942399935cba715d6969589ceef
Maximilian Wolf <69987866+MaxWolf-01@users.noreply.github.com>
2024-11-11 22:03:09 +0100

    good state

--- examples/train_dist.py
+++ examples/train_dist.py
@@ -1,8 +1,10 @@
 from __future__ import annotations, annotations
 
+import argparse
 import asyncio
 import multiprocessing
 import pickle
+import typing
 from dataclasses import dataclass
 from datetime import datetime
 from pathlib import Path
@@ -13,15 +15,15 @@ import gymnasium as gym
 import numpy as np
 import torch
 import wandb
-from es_torch.distributed import distributed_pb2 as proto, distributed_pb2_grpc as services
 from google.protobuf import timestamp_pb2
 from gymnasium import VectorizeMode
 
 from es_torch.distrbuted_optim import Config as ESConfig, ES
+from es_torch.distributed import distributed_pb2 as proto, distributed_pb2_grpc as services
 from examples.policies import SimpleMLP, SimpleMLPConfig
 from examples.utils import (
-    ExperimentConfig,
-    WandbConfig,
+    ESArgumentHandler, ExperimentConfig,
+    WandbArgumentHandler, WandbConfig,
     reshape_params,
 )
 
@@ -64,26 +66,24 @@ class Config(ExperimentConfig):
         )
 
 
+class WorkerState(typing.NamedTuple):
+    epoch: int
+    optim: ES
+    wandb_run: wandb.sdk.wandb_run.Run | None
+
+
 class Worker:
     worker_id: int
-    optim: ES
+    state: WorkerState | None
 
     def __init__(self, config: Config, server_address: str) -> None:
         self.config = config
         self.channel = grpc.insecure_channel(server_address)
         self.stub = services.ESServiceStub(self.channel)
-        self._epoch_count = 0
 
     async def run(self) -> None:
-        num_cpus = multiprocessing.cpu_count()
-        self.worker_id = self.stub.Hello(proto.HelloRequest(num_cpus=num_cpus)).id
-        # TODO
-        # resp = self.stub.GetState(es_pb2.GetStateRequest(id=self.worker_id))
-        # self.optim = pickle.loads(resp.state)
-
         heartbeat_task = asyncio.create_task(self._send_heartbeats())
         subscribe_task = asyncio.create_task(self._handle_server_events())
-
         try:
             await asyncio.gather(heartbeat_task, subscribe_task)
         except Exception as e:
@@ -96,38 +96,62 @@ class Worker:
                 timestamp = timestamp_pb2.Timestamp()
                 timestamp.FromDatetime(datetime.now())
                 self.stub.Heartbeat(proto.HeartbeatRequest(id=self.worker_id, timestamp=timestamp))
-                await asyncio.sleep(5)
+                await asyncio.sleep(10)
             except Exception as e:
                 print(f"Heartbeat error: {e} on worker {self.worker_id}")
                 await asyncio.sleep(1)
 
     async def _handle_server_events(self) -> None:
         try:
-            subscribe_request = proto.SubscribeRequest(id=self.worker_id)
-            responses = self.stub.Subscribe(subscribe_request)
+            num_cpus = multiprocessing.cpu_count()
+            subscribe_res = self.stub.Subscribe(proto.SubscribeRequest(num_cpus=num_cpus))
+            responses = self.stub.Subscribe(subscribe_res)
+            response_funcs = {
+                proto.ServerEventType.HELLO: self._handle_hello,
+                proto.ServerEventType.EVALUATE_BATCH: self._handle_evaluate_batch,
+                proto.ServerEventType.OPTIM_STEP: self._handle_optim_step,
+                proto.ServerEventType.SEND_STATE: self._handle_send_state,
+            }
             for res in responses:  # loops indefinitely
-                if self._epoch_count >= self.config.epochs:
+                if self.state.epoch >= self.config.epochs:
                     break
-                match res.type:
-                    case proto.ServerEventType.EVALUATE_BATCH:
-                        perturbed_params = self.optim.get_perturbed_params()
-                        policy_batch_slice = slice(res.eval_pop_slice.start, res.eval_pop_slice.end)
-                        results = self.evaluate_policy_batch(perturbed_params[policy_batch_slice, :])
-                        self.stub.Done(proto.DoneRequest(id=self.worker_id, reward_batch=results.numpy().tobytes()))
-                    case proto.ServerEventType.OPTIM_STEP:
-                        rewards = torch.frombuffer(res.optim_rewards, dtype=torch.float32)
-                        self.optim.step(rewards=rewards)
-                        self._epoch_count += 1
-                    case proto.ServerEventType.SEND_STATE:  # a new worker joins and needs the current state
-                        # TODO also requires epoch count
-                        self.stub.SendState(proto.SendStateRequest(id=self.worker_id, state=pickle.dumps(self.optim)))
-                    case _:
-                        print(f"Unknown event type: {res.type}")
+                response_funcs[res.type](res)  # noqa
+
         except Exception as e:
             print(f"Subscription error: {e}")
             await asyncio.sleep(1)
 
-    def evaluate_policy_batch(self, policy_params_batch: torch.Tensor) -> torch.Tensor:
+    def _handle_hello(self, res: proto.ServerEventType.HELLO) -> None:
+        self.worker_id = res.id
+        self.state: WorkerState | None = pickle.loads(res.init_state) if res.init_state else None
+        if self.state is None:
+            initial_params = torch.nn.utils.parameters_to_vector(SimpleMLP(self.config.policy).parameters())
+            optim = ES(self.config.es, params=initial_params)
+            run = wandb.init(
+                project=self.config.wandb.project,
+                name=self.config.wandb.name,
+                tags=self.config.wandb.tags,
+                entity=self.config.wandb.entity,
+                config=vars(self.config),
+            ) if self.config.wandb.enabled else None
+            self.state = WorkerState(epoch=0, optim=optim, wandb_run=run)
+
+    def _handle_evaluate_batch(self, res: proto.EvaluateBatchEvent) -> None:
+        perturbed_params = self.state.optim.get_perturbed_params()
+        policy_batch_slice = slice(res.pop_slice.start, res.pop_slice.end)
+        results = self._evaluate_policy_batch(perturbed_params[policy_batch_slice, :])
+        self.stub.Done(proto.DoneRequest(id=self.worker_id, reward_batch=results.numpy().tobytes()))
+
+    def _handle_optim_step(self, res: proto.OptimStepEvent) -> None:
+        rewards = torch.frombuffer(res.rewards, dtype=torch.float32)
+        self.state.optim.step(rewards)
+        self.state.epoch += 1
+
+    def _handle_send_state(self, res: proto.SendStateEvent) -> None:
+        # a new worker joins and needs the current state
+        self.stub.SendState(proto.SendStateRequest(self.worker_id, pickle.dumps(self.state)))
+
+    def _evaluate_policy_batch(self, policy_params_batch: torch.Tensor) -> torch.Tensor:
         env = gym.make_vec("Humanoid-v5", num_envs=self.config.es.n_pop, vectorization_mode=VectorizeMode.ASYNC)
 
         obs, _ = env.reset(seed=self.config.env_seed)
@@ -171,3 +195,45 @@ def train(config: Config):
     finally:
         if config.wandb.enabled:
             wandb.finish()
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--epochs", type=int, help="Number of training epochs")
+    parser.add_argument("--max_episode_steps", type=int, help="Max steps per episode")
+    parser.add_argument("--hid", type=int, help="Hidden layer size")
+    parser.add_argument("--ckpt", type=int, help="Save every N epochs. N<=0 disables saving")
+    parser.add_argument("--server_ip", type=str, help="IP address of the server", default="localhost:8080")
+    ESArgumentHandler.add_args(parser)
+    WandbArgumentHandler.add_args(parser)
+    args = vars(parser.parse_args())
+    cfg = Config.default()
+    ESArgumentHandler.update_config(args, cfg)
+    WandbArgumentHandler.update_config(args, cfg)
+    cfg.epochs = args["epochs"] or cfg.epochs
+    cfg.max_episode_steps = args["max_episode_steps"] or cfg.max_episode_steps
+    cfg.policy.hidden_dim = args["hid"] or cfg.policy.hidden_dim
+
+    # if cfg.wandb.enabled:
+
+    # filename = "humanoid.pt" if not cfg.wandb.enabled else f"humanoid_{run.name}.pt"
+    # cfg.ckpt_path = Paths.CKPTS / filename
+    # cfg.ckpt_every = args["ckpt"] or cfg.ckpt_every
+    #
+    # pprint(cfg)
+
+    # final_params = train(cfg)
+
+    # model = SimpleMLP(cfg.policy)
+    # torch.nn.utils.vector_to_parameters(final_params, model.parameters())
+    # fp = cfg.ckpt_path.with_stem(cfg.ckpt_path.stem + "_final")
+    # save_policy(
+    #     model=model,
+    #     model_config=cfg.policy,
+    #     fp=fp,
+    # )
+    # print(f"Saved final checkpoint to {fp}")
+
+
+if __name__ == "__main__":
+    main()
