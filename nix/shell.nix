899ff7134a170c3b40bf7bc38c274b31f1ee6553
Lukas Leeb <git@lukasl.dev>
2024-11-12 19:31:21 +0100

    update nix shell

--- nix/shell.nix
+++ nix/shell.nix
@@ -2,7 +2,7 @@
   pkgs ? import <nixpkgs> { },
 }:
 
-pkgs.mkShell {
+pkgs.mkShell rec {
   packages = with pkgs; [
     go_1_23
     gopls
@@ -13,9 +13,50 @@ pkgs.mkShell {
     grpcurl
     buf
     buf-language-server
+
+    uv
+    python312
+
+    openssl
+
+    zlib
+    glib
+    stdenv.cc.cc
+    python312
+    uv
+    clang
+    git
+    gitRepo
+    gnupg
+    autoconf
+    curl
+    procps
+    gnumake
+    util-linux
+    m4
+    gperf
+    unzip
+    libGLU
+    libGL
+    fontconfig
+    xorg.libXi
+    xorg.libXmu
+    freeglut
+    freetype
+    dbus
+    xorg.libXext
+    xorg.libX11
+    xorg.libXv
+    xorg.libXrandr
+    xorg.libxcb
+    libxkbcommon
+    zlib
+    ncurses5
+    stdenv.cc
+    binutils
   ];
 
   shellHook = ''
-    ${pkgs.just}/bin/just setup
+    export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath packages}
   '';
 }
